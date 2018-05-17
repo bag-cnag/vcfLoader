4f03311516657e70a59fc93ec4ea1063d5767c0d
Cristina Luengo <cristinaluengoagullo@gmail.com>
2018-05-17 15:52:55 +0200

    Update variants folder name

--- python/rdconnect/transform.py
+++ python/rdconnect/transform.py
@@ -41,4 +41,4 @@ def transform(dataset,destination,chrom):
                                                         rs : orElse(va.rs,'.')
                                                         }]''']
                         ).annotate_variants_expr(['va.vep = let c= va.vep in drop(va.vep,colocated_variants,motif_feature_consequences,intergenic_consequences,regulatory_feature_consequences,most_severe_consequence,variant_class, assembly_name,allele_string,ancestral,context,end,id,input,seq_region_name,start,strand)',])\
-        .variants_table().to_dataframe().write.mode('overwrite').save(destination+"/variants/chrom="+chrom)
+        .variants_table().to_dataframe().write.mode('overwrite').save(destination+"/variants_merged/chrom="+chrom)
