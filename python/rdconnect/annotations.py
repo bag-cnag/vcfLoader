800bb8c96349d9edd9d23ba494729fabbf973df1
Cristina Luengo <cristinaluengoagullo@gmail.com>
2018-09-04 10:32:00 +0200

    Repartition to min_partitions

--- python/rdconnect/annotations.py
+++ python/rdconnect/annotations.py
@@ -1,18 +1,17 @@
 from rdconnect import utils
 
 def annotationsVEP(hc,source, destinationPath, vepPath,number_partitions):
-    variants= hc.read(source)
+    variants = hc.read(source)
     print("running vep")
     varAnnotated= variants.vep(vepPath)
     print("destination is "+destinationPath)
-    varAnnotated.repartition(number_partitions).split_multi().write(destinationPath,overwrite=True)
+    varAnnotated.split_multi().write(destinationPath,overwrite=True)
 
 def importDBTable(hc,sourcePath,destinationPath,number_partitions):
     print("source Path is "+sourcePath)
-    dbnsfpTable = hc.import_table(sourcePath).annotate('variant= Variant(`#chr`,`pos(1-coor)`.toInt,`ref`,`alt`)').key_by('variant')
+    dbnsfpTable = hc.import_table(sourcePath,min_partitions=number_partitions).annotate('variant= Variant(`#chr`,`pos(1-coor)`.toInt,`ref`,`alt`)').key_by('variant')
     dbnsfpTable.rename({'1000Gp1_AF':'Gp1_AF1000','1000Gp1_EUR_AF':'Gp1_EUR_AF1000','1000Gp1_ASN_AF':'Gp1_ASN_AF1000','1000Gp1_AFR_AF':'Gp1_AFR_AF1000','GERP++_RS':'GERP_RS'}) \
                .select(['variant','Gp1_AF1000','Gp1_EUR_AF1000','Gp1_ASN_AF1000','Gp1_AFR_AF1000','GERP_RS','MutationTaster_score','MutationTaster_pred','phyloP46way_placental','Polyphen2_HDIV_pred','Polyphen2_HVAR_score','SIFT_pred','SIFT_score']) \
-               .repartition(number_partitions) \
                .write(destinationPath,overwrite=True) 
 
 def annotatedbnsfp(hc,variants, dbsfp_path,destinationPath):
@@ -21,7 +20,7 @@ def annotatedbnsfp(hc,variants, dbsfp_path,destinationPath):
 
 def importDBvcf(hc,sourcePath,destinationPath,number_partitions):
     print("cadd source Path is "+sourcePath)
-    dbnsfpTable=hc.import_vcf(sourcePath).repartition(number_partitions).write(destinationPath,overwrite=True)
+    dbnsfpTable=hc.import_vcf(sourcePath,min_partitions=number_partitions).write(destinationPath,overwrite=True)
     
 def annotateVCF(hc,variants,annotationPath,destinationPath,annotations):
     cadd = hc.read(annotationPath).split_multi()
