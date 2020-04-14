4f06e78d9c4f76ba72e4c1ed14f7397811bf7404
dpiscia <davidepi79@gmail.com>
2020-04-14 14:10:44 +0200

    comb fam

--- python/rdconnect/combine.py
+++ python/rdconnect/combine.py
@@ -286,7 +286,7 @@ def createDenseMatrix( sc, sq, url_project, prefix_hdfs, max_items_batch, dense_
             small_matrix_sparse = sparse_matrix.filter_cols( sam.contains( sparse_matrix['s'] ) )
             
             #write slice of sparse matrix (try also without writing and reading)
-            path = '{0}/temp/chrm-{1}'.format( dm, chrom )
+            path = 'temp/{0}/temp/chrm-{1}'.format( dm, chrom )
             small_matrix_sparse.write( path, overwrite = True )
             small_matrix=hl.read_matrix_table(path)
             
