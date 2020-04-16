04f22d10b672cf872374aabd714f2a17b90decb1
dpiscia <davidepi79@gmail.com>
2020-04-16 20:31:46 +0200

    sliece

--- python/rdconnect/combine.py
+++ python/rdconnect/combine.py
@@ -395,7 +395,7 @@ def getExperimentsByFamily( pids, url_project, id_gpap, token_gpap, sort_output 
     if sort_output:
         return sorted( rst, key = lambda x: x[ 2 ] )
     else:
-        return rst[1600:9000]
+        return rst[3200:9000]
 
 def divideChunks( collection, size ): 
     # looping till length l 
