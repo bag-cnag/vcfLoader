0ad179135c9de7535a51a73784b020b36f86a0a1
dpiscia <davidepi79@gmail.com>
2020-06-22 15:29:31 +0200

    matrix

--- python/rdconnect/annotations.py
+++ python/rdconnect/annotations.py
@@ -97,7 +97,7 @@ def loadDenseMatrix( hl, originPath, sourcePath, destinationPath, nPartitions ):
         lgr.debug( 'Contents in "{}" will be overwritten'.format( destinationPath ) )
         #vcf = vcf.rows()
         vcf = vcf.key_rows_by( vcf.locus, vcf.alleles )
-        vcf = vcf.distinct().write( destinationPath, overwrite = True )
+        vcf = vcf.distinct_by_row().write( destinationPath, overwrite = True )
     except Exception as ex:
         lgr.debug( 'Unexpected error during the load of dense matrix "{}"'.format( sourcePath ) )
         lgr.error( 'Unexpected error --> {}'.format( str( ex ) ) )
