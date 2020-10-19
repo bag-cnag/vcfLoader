5f5537124759f53a5ae19ef2c4b6c393ac85d85f
Carles Hernandez <2046180+carleshf@users.noreply.github.com>
2020-10-19 09:33:38 +0200

    Propagated changes to dbnsfp.

--- python/rdconnect/annotateGenetics.py
+++ python/rdconnect/annotateGenetics.py
@@ -101,7 +101,6 @@ def vep(self = None, config = None, hl = None, log = None):
 	-------
 	The function returns a 'GenomicData' object annotated with VEP.
 	"""
-
 	isSelf = True
 	if self is None:
 		isSelf = False
@@ -222,19 +221,36 @@ def dbnsfp(self, config, hl = None, log = None):
 		Set it to None to load the dataset from 'source_path'. If a GenomicData
 		is assigned to this argument, no set is loaded from 'source_path' and
 		the argument is ignored.
-	config: ConfigFile, mandatory
-		Configuration for this step of the pipeline.
+	config: ConfigFile, optional
+		Configuration for this step of the pipeline. If not provided or set to
+		None the configuration is looked into the GenomicData in self.
 	hl: context, optional
-		HAIL context.
+		HAIL context. If not provided or set to None the reference to the 
+		module is looked into the GenomicData in self.
 	log: logger, optional
-		A logger to have track of the steps used in the loading process.
+		A logger to have track of the steps used in the loading process. If not
+		provided or set to None the logger is looked into the GenomicData in 
+		self. If no logger is in the provided nor in the GenomicData, then no
+		log is performed.
 
 	Returns
 	-------
 	The function returns a 'GenomicData' object annotated with dbSNFP.
 	"""
-	if log is not None:
-		log.info('Entering annotation step "dbSNFP"')
+	isSelf = True
+	if self is None:
+		isSelf = False
+
+	self, isConfig, isHl = _check_class_and_config(self, config, hl, log)
+	self.log.info('Entering annotation step "dbSNFP"')
+
+	if not isConfig:
+		self.log.error('No configuration was provided')
+		raise NoConfigurationException('No configuration was provided')
+
+	if not isHl:
+		self.log.error('No pointer to HAIL module was provided')
+		raise NoHailContextException('No pointer to HAIL module was provided')
 
 	source_file = utils.create_chrom_filename(config['process/source_file'], config['process/chrom'])
 	source_path = utils.create_chrom_filename(config['process/source_path'], config['process/chrom'])
@@ -243,42 +259,33 @@ def dbnsfp(self, config, hl = None, log = None):
 	dbnsfp_path = utils.create_chrom_filename(config['annotation/clean/dbNSFP'], config['process/chrom'])
 	autosave = config['process/autosave']
 
-	if self is None and log is not None:
-		log.debug('> Argument "self" was not set')
-	if self is not None and log is not None:
-		log.debug('> Argument "self" was set')
-	if log is not None:
-		log.debug('> Argument "source_file" filled with "{}"'.format(source_file))
-		log.debug('> Argument "source_path" filled with "{}"'.format(source_path))
-		log.debug('> Argument "destination_path" filled with "{}"'.format(destination_path))
-		log.debug('> Argument "dbnsfp_path" filled with "{}"'.format(dbnsfp_path))
-		
-	if autosave and log is not None:
-		log.debug('> Argument "autosave" was set')
-	if not autosave and log is not None:
-		log.debug('> Argument "autosave" was not set')
+	self.log.debug('> Argument "self" was set' if isSelf else '> Argument "self" was not set')
+	self.log.debug('> Argument "source_file" filled with "{}"'.format(source_file))
+	self.log.debug('> Argument "source_path" filled with "{}"'.format(source_path))
+	self.log.debug('> Argument "destination_path" filled with "{}"'.format(destination_path))
+	self.log.debug('> Argument "dbnsfp_path" filled with "{}"'.format(dbnsfp_path))
+	self.log.debug('> Argument "autosave" was set' if autosave else '> Argument "autosave" was not set')
 
 	dbnsfp = hl.read_table(dbnsfp_path)
 	if self is None:
-		self = GenomicData()
 		self.data = hl.methods.read_matrix_table(source_path)
 		self.state = []
 		self.file = []
 
 	self.data = self.data.annotate_rows(
-		gp1_asn_af=hl.or_else(_removeDot(hl,dbnsfp[self.data.locus, self.data.alleles].Gp1_ASN_AF1000,"6"), 0.0),
-		gp1_eur_af=hl.or_else(_removeDot(hl,dbnsfp[self.data.locus, self.data.alleles].Gp1_EUR_AF1000,"6"), 0.0),
-		gp1_afr_af=hl.or_else(_removeDot(hl,dbnsfp[self.data.locus, self.data.alleles].Gp1_AFR_AF1000,"6"), 0.0),
-		gp1_af=hl.or_else(_removeDot(hl,dbnsfp[self.data.locus, self.data.alleles].Gp1_AF1000,"6"), 0.0),
-		gerp_rs=dbnsfp[self.data.locus, self.data.alleles].GERP_RS,
-		mt=hl.or_else(hl.max(dbnsfp[self.data.locus, self.data.alleles].MutationTaster_score.split(";").map(lambda x:_removeDot(hl,x,"4"))),0.0),
-		mutationtaster_pred=_mt_pred_annotations(hl,dbnsfp[self.data.locus, self.data.alleles]),
-		phyloP46way_placental=_removeDot(hl,dbnsfp[self.data.locus, self.data.alleles].phyloP46way_placental,"4"),
-		polyphen2_hvar_pred=_polyphen_pred_annotations(hl,dbnsfp[self.data.locus, self.data.alleles]),
-		polyphen2_hvar_score=hl.or_else(hl.max(dbnsfp[self.data.locus, self.data.alleles].Polyphen2_HVAR_score.split(";").map(lambda x: _removeDot(hl,x,"4"))),0.0),
-		sift_pred=_sift_pred_annotations(hl,dbnsfp[self.data.locus, self.data.alleles]),
-		sift_score=hl.or_else(hl.max(dbnsfp[self.data.locus, self.data.alleles].SIFT_score.split(";").map(lambda x: _removeDot(hl,x,"4"))),0.0),
-		cosmic_id=dbnsfp[self.data.locus, self.data.alleles].COSMIC_ID)
+		gp1_asn_af = hl.or_else(_removeDot(hl, dbnsfp[self.data.locus, self.data.alleles].Gp1_ASN_AF1000, '6'), 0.0),
+		gp1_eur_af = hl.or_else(_removeDot(hl, dbnsfp[self.data.locus, self.data.alleles].Gp1_EUR_AF1000, '6'), 0.0),
+		gp1_afr_af = hl.or_else(_removeDot(hl, dbnsfp[self.data.locus, self.data.alleles].Gp1_AFR_AF1000, '6'), 0.0),
+		gp1_af = hl.or_else(_removeDot(hl, dbnsfp[self.data.locus, self.data.alleles].Gp1_AF1000, '6'), 0.0),
+		gerp_rs = dbnsfp[self.data.locus, self.data.alleles].GERP_RS,
+		mt = hl.or_else(hl.max(dbnsfp[self.data.locus, self.data.alleles].MutationTaster_score.split(';').map(lambda x:_removeDot(hl, x, '4'))), 0.0),
+		mutationtaster_pred = _mt_pred_annotations(hl, dbnsfp[self.data.locus, self.data.alleles]),
+		phyloP46way_placental = _removeDot(hl, dbnsfp[self.data.locus, self.data.alleles].phyloP46way_placental, '4'),
+		polyphen2_hvar_pred = _polyphen_pred_annotations(hl, dbnsfp[self.data.locus, self.data.alleles]),
+		polyphen2_hvar_score = hl.or_else(hl.max(dbnsfp[self.data.locus, self.data.alleles].Polyphen2_HVAR_score.split(';').map(lambda x: _removeDot(hl, x, '4'))), 0.0),
+		sift_pred = _sift_pred_annotations(hl, dbnsfp[self.data.locus, self.data.alleles]),
+		sift_score = hl.or_else(hl.max(dbnsfp[self.data.locus, self.data.alleles].SIFT_score.split(';').map(lambda x: _removeDot(hl, x, '4'))), 0.0),
+		cosmic_id = dbnsfp[self.data.locus, self.data.alleles].COSMIC_ID)
 
 	self.state = ['dbNSFP'] + self.state
 	if autosave and destination_path != '':
