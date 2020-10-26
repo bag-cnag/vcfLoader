622ecaad9663a2a89a4d0c58c0a46c172aa8844d
Carles Hernandez <2046180+carleshf@users.noreply.github.com>
2020-10-26 17:32:45 +0100

    Solved issue with internal variables and external methods in CnfigFile class.

--- python/rdconnect/classConfig.py
+++ python/rdconnect/classConfig.py
@@ -61,43 +61,44 @@ def _set_nested(dic, key_list, value):
 class ConfigFile:
 
 	def __init__(self):
-		self.config_path = None
-		self.data = {}
+		self._config_path = None
+		self._data = {}
+		self._keys = []
 
 	def __getitem__(self, key):
-		if key in self.keys:
-			return _get_from_dict(self.data, key.split('/'))
+		if key in self._keys:
+			return _get_from_dict(self._data, key.split('/'))
 		else:
 			return None
 
 	def __setitem__(self, key, value):
-		_set_nested(self.data, key.split('/'), value)
-		self.keys = _get_keys_dict(self.data)
+		_set_nested(self._data, key.split('/'), value)
+		self._keys = _get_keys_dict(self._data)
 
 	def __str__(self):
-		return 'ConfigFile<{}>'.format(str(self.config_path))
+		return 'ConfigFile<{}>'.format(str(self._config_path))
 
 	def get(self, key):
 		return self[key]
 
 	def overwrite(self, key, value):
 		new = deepcopy(self)
-		_set_nested(new.data, key.split('/'), value)
-		new.keys = _get_keys_dict(new.data)
+		_set_nested(new._data, key.split('/'), value)
+		new._keys = _get_keys_dict(new._data)
 		return new
 
-	def keys(sekf):
-		return _get_keys_dict(self.data)
+	def keys(self):
+		return _get_keys_dict(self._data)
 
 	@staticmethod
 	def from_file(config_path):
 		x = ConfigFile()
-		x.config_path = config_path
-		x.data = {}
-		with open(config_path) as data_file:
-			x.data = json.load(data_file)
+		x._config_path = config_path
+		x._data = {}
+		with open(_config_path) as data_file:
+			x._data = json.load(data_file)
 		try:
-			x.keys = _get_keys_dict(x.data)
+			x._keys = _get_keys_dict(x._data)
 		except Exception as ex:
 			raise Exception('Invalid configuration file provided.\n{}\n{}'.format(str(ex), str(format_exc())))
 		return x
@@ -105,10 +106,10 @@ class ConfigFile:
 	@staticmethod
 	def from_str(data_str):
 		x = ConfigFile()
-		x.config_path = None
-		x.data = json.loads(data_str)
+		x._config_path = None
+		x._data = json.loads(data_str)
 		try:
-			x.keys = _get_keys_dict(x.data)
+			x._keys = _get_keys_dict(x._data)
 		except Exception as ex:
 			raise Exception('Invalid configuration file provided.\n{}\n{}'.format(str(ex), str(format_exc())))
 		return x
