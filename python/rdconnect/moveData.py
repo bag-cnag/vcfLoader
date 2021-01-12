6a844bcac90a48545c1144f923681956b4810c4b
a <2046180+carleshf@users.noreply.github.com>
2021-01-12 14:39:40 +0100

    ..

--- python/rdconnect/moveData.py
+++ python/rdconnect/moveData.py
@@ -76,6 +76,8 @@ def gvcf(config, log = VoidLog(), batch = 10):
 	}
 	data = "{\"page\": 1, \"pageSize\": " + str(batch) + ", \"fields\": [\"RD_Connect_ID_Experiment\",\"mapping\",\"variantCalling\",\"genomicsdb\",\"hdfs\",\"es\",\"in_platform\"], \"sorted\":[{\"id\":\"RD_Connect_ID_Experiment\",\"desc\":false}], \"filtered\":[{\"id\":\"variantCalling\",\"value\":\"pass\"},{\"id\":\"rohs\",\"value\":\"pass\"},{\"id\":\"in_platform\",\"value\":\"waiting\"}]}"
 
+	print(data)
+	print(headers)
 	log.debug('> Querying DM using url "{0}"'.format(url))
 	
 	response = requests.post(url, data = data, headers = headers, verify = False)
