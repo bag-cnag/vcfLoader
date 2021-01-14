dc18be58269d90135913089ea41ae3675c6be787
a <2046180+carleshf@users.noreply.github.com>
2021-01-14 20:28:00 +0100

    ..

--- python/rdconnect/structuredMatrix.py
+++ python/rdconnect/structuredMatrix.py
@@ -1,1 +1,11 @@
 import requests
+import rdconnect.utils as utils
+from rdconnect.classException import *
+
+
+
+"""structuredMatrix
+
+This module contains the functions used to create a sparse matrix and to append
+experiments to an already existing sparse matrix.
+"""
