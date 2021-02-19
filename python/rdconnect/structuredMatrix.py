c4cfeddbdecfaf467a59408e999cecaa8b7d8993
a <2046180+carleshf@users.noreply.github.com>
2021-02-19 08:12:03 +0100

    Testing 'split_multi_hts' over loaded vcfs.

--- python/rdconnect/structuredMatrix.py
+++ python/rdconnect/structuredMatrix.py
@@ -205,8 +205,8 @@ def _load_gvcf(hl, experiments, version_path, previous_version_path, chrom, part
 			reference_genome = interval[ 'reference_genome' ], 
 			array_elements_required = interval[ 'array_elements_required' ]
 		)
-		x = hl.split_multi_hts(x, keep_star = False)
-		return x
+		z = [ hl.split_multi_hts(z, keep_star = False) for x in x ]
+		return z
 
 	interval = utils.get_chrom_intervals(chrom, partitions, hl)
 	vcfs = [ transformFile(mt) for mt in importFiles([ x[ 'file' ] for x in experiments ]) ]
