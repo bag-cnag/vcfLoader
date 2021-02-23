533d7224f9916cb75d40fbe4c008e18dcacd8574
a <2046180+carleshf@users.noreply.github.com>
2021-02-23 10:19:01 +0100

    Updated 'sample_in_platform'.

--- python/rdconnect/samplesTracking.py
+++ python/rdconnect/samplesTracking.py
@@ -39,9 +39,9 @@ def sample_index(self = None, config = None, hl = None, log = None):
 		self.log.error('No configuration was provided')
 		raise NoConfigurationException('No configuration was provided')
 
-	if not 'process/chrom' in self.config.keys():
-		log.warning('Provided configuration with no chromosome attached ("process/chrom"). Chromosome 21 will be used.')
-		self.config['process/chrom'] = 21
+	if not 'process/chrom' in self.config.keys() ot str(self.config['process/chrom']) != '21':
+		log.warning('Provided configuration with no chromosome attached ("process/chrom") or it was not chromosome 21. Chromosome 21 will be used.')
+		self.config['process/chrom'] = '21'
 
 	source_file = utils.create_chrom_filename(self.config['process/source_file'], self.config['process/chrom'])
 	source_path = utils.create_chrom_filename(self.config['process/source_path'], self.config['process/chrom'])
@@ -123,23 +123,26 @@ def sample_in_platform(self = None, config = None, hl = None, log = None):
 		isSelf = False
 
 	self, isConfig, isHl = utils.check_class_and_config(self, config, hl, log)
-	self.log.info('Entering updating step "DM - INDEX"')
+	self.log.info('Entering updating step "DM - IN_PLATFORM"')
 
 	if not isConfig:
 		self.log.error('No configuration was provided')
 		raise NoConfigurationException('No configuration was provided')
 
-	if not 'process/chrom' in self.config.keys():
-		log.warning('Provided configuration with no chromosome attached ("process/chrom"). Chromosome 21 will be used.')
-		self.config['process/chrom'] = 21
+	if not 'process/chrom' in self.config.keys() ot str(self.config['process/chrom']) != '21':
+		log.warning('Provided configuration with no chromosome attached ("process/chrom") or it was not chromosome 21. Chromosome 21 will be used.')
+		self.config['process/chrom'] = '21'
 
-	source_file = utils.create_chrom_filename(self.config['process/source_file'], self.config['process/chrom'])
-	source_path = utils.create_chrom_filename(self.config['process/source_path'], self.config['process/chrom'])
-	source_path = os.path.join(source_path, source_file)
+	if self is None:
+		source_file = utils.create_chrom_filename(self.config['process/source_file'], self.config['process/chrom'])
+		source_path = utils.create_chrom_filename(self.config['process/source_path'], self.config['process/chrom'])
+		source_path = os.path.join(source_path, source_file)
 
-	self.log.debug('> Argument "self" was set' if isSelf else '> Argument "self" was not set')
-	self.log.debug('> Argument "source_path" filled with "{}"'.format(source_path))
-	self.log.debug('> Argument "index_name" filled with "{}"'.format(self.config['resources/elasticsearch/index_name']))
+		self.log.debug('> Argument "self" was set' if isSelf else '> Argument "self" was not set')
+		self.log.debug('> Argument "source_path" filled with "{}"'.format(source_path))
+		self.log.debug('> Argument "index_name" filled with "{}"'.format(self.config['resources/elasticsearch/index_name']))
+
+		self.data = self.hl.methods.read_matrix_table(source_path)
 
 	url = 'https://' + self.config['applications/datamanagement/api_exp_status'].format(self.config['applications/datamanagement/ip'], self.config['applications/datamanagement/api_group'])
 	headers = { 'accept': 'application/json', 
@@ -148,10 +151,7 @@ def sample_in_platform(self = None, config = None, hl = None, log = None):
 		'Host': self.config['applications/datamanagement/host'] }
 	data = "{\"in_platform\":\"pass\"}"
 	
-	# vcf = self.hl.split_multi_hts(self.hl.import_vcf(str(source_path), array_elements_required = False, force_bgz = True, min_partitions = 2))
-	# full_samples = [y.get('s') for y in vcf.col.collect()]
-	mtbl = self.hl.methods.read_matrix_table(source_path)
-	full_samples = [ y.get('s') for y in mtbl.col.collect() ]
+	full_samples = [ y.get('s') for y in self.data.col.collect() ]
 
 	self.log.info('> Experiments in loaded VCF: {}'.format(len(full_samples)))
 	self.log.debug('> First and last sample: {} // {}'.format(full_samples[0], full_samples[len(full_samples) - 1]))
