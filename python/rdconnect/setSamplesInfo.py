00ee0ff7d9da928abf8683ce09f3e12b2c463b42
a <2046180+carleshf@users.noreply.github.com>
2021-02-25 11:01:03 +0100

    Solving issues when using set_experimet.

--- python/rdconnect/setSamplesInfo.py
+++ python/rdconnect/setSamplesInfo.py
@@ -69,7 +69,7 @@ def set_experiment(self = None, config = None, hl = None, log = VoidLog(), is_pl
 	self.log.debug('> Querying {0} experiments using url "{1}"'.format(str(len(samples)), url))
 	
 	for ii, sam in enumerate(samples):
-		q_url = url + '?experiment=' + sam
+		q_url = url + '&experiment=' + sam
 		response = requests.post(q_url, data = data, headers = headers, verify = False)
 		if response.status_code != 200:
 			self.log.error('Query #{} for experiment {} resulted in a {} message'.format(str(ii), sam, str(response.status_code)))
