63765b050a918be6242e6e98e5a7e8ad97e8f9ea
a <2046180+carleshf@users.noreply.github.com>
2021-03-05 17:11:11 +0100

    Added prints.

--- python/rdconnect/structuredMatrix.py
+++ python/rdconnect/structuredMatrix.py
@@ -196,9 +196,10 @@ def append_to_sparse_matrix(self = None, config = None, hl = None, log = VoidLog
 		revisions_to_collect = [ version ] + revisions_to_collect
 
 	self.log.info('Starting step 2 - merging {} cumulative matrices'.format(len(revisions_to_collect)))
-	print(revisions_to_collect)
+	print("revisions_to_collect", revisions_to_collect)
 	
 	for ii in range(1, len(revisions_to_collect)):
+		print(revisions_to_collect[ ii-1 ], revisions_to_collect[ ii ], chrom)
 		last = _combine_mt(self.hl, base, revisions_to_collect[ ii-1 ], revisions_to_collect[ ii ], utils.version_bump(revisions_to_collect[ ii ][ 0 ], 'version'), chrom)
 
 	self.data = last
